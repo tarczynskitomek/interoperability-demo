672d25278bf101519a50f18d4b58362025570758
Tomasz Tarczyński <tarczynskitomek@gmail.com>
2023-06-29 22:06:25 +0200

    Remove uneccessary static

--- src/main/java/it/tarczynski/interoperability/demo/ErrorPolicyClient.java
+++ src/main/java/it/tarczynski/interoperability/demo/ErrorPolicyClient.java
@@ -5,9 +5,6 @@ import it.tarczynski.interoperability.demo.error.Policies;
 
 class ErrorPolicyClient {
 
-    private static void consumer(ErrorPolicy errorPolicy) {
-    }
-
     public void use() {
         // this is legal in Java
         consumer(
@@ -19,5 +16,8 @@ class ErrorPolicyClient {
         // this is illegal though:
         // final InternalErrorHandlerPolicy police = Policies
         //        .internalErrorPolicy().build();
+    }
+
+    private void consumer(ErrorPolicy errorPolicy) {
     }
 }
